3715b433baa7582c9ecab4afbef496e0ab8fe6d2
Gabriel <shalitmontagne.g@gmail.com>
2022-04-20 15:05:50 -0600

    Add basic react signup form with POST

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  let data = {
+    "username": username,
+    "password": password,
+    "password_confirmation": confirmPassword
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    fetch('/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+      
+    })
+
+    // if (password === confirmPassword) {
+    //   console.log('They match')
+    // } else {
+    //   console.log('Passwords do not match')
+    // }
+
+
+
+    // console.log(username, password, confirmPassword)
+    // setUsername('')
+    // setPassword('')
+    // setConfirmPassword('')
+  }
+
+  //helpers
+
+  function handleUserName(e) {
+    setUsername(e.target.value)
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value)
+  }
+
+  function handleConfirmPassword(e) {
+    setConfirmPassword(e.target.value)
+  }
+
+  return(
+    <div>
+      <form>
+        <label className="label">Username</label>
+        <input onChange={handleUserName} className="input"
+        value={username} type="text" />
+
+        <label className="label">Password</label>
+        <input onChange={handlePassword} className="input"
+        value={password} type="password" />
+
+        <label className="label">Confirm Password</label>
+        <input onChange={handleConfirmPassword} className="input"
+        value={confirmPassword} type="password" />
+
+        <button onClick={handleSubmit} className="btn" type="submit">
+        Submit
+        </button>
+      </form>
+      
     </div>
-  );
+  )
 }
 
 export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get '/users', to: 'users#index'
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
 
